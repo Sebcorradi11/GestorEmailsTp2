95073f0968d3c982f915ccd90fe790827e965af8
Sebcorradi11 <sebastianocorradi57@gmail.com>
2023-10-08 18:13:20 -0300

    Clase enviar correo

--- src/main/java/email/ucp/EnviarCorreo.java
+++ src/main/java/email/ucp/EnviarCorreo.java
@@ -1,5 +1,12 @@
 package email.ucp;
+import java.util.List;
+import java.util.ArrayList;
 
 public class EnviarCorreo {
-    
+    public static void enviar (Correo correo, Buzon buzonRemitente, List<Buzon> buzonesDestinatarios){
+    buzonRemitente.agregarAEnviados(correo);
+    for (Buzon buzonDestinatario : buzonesDestinatarios){
+        buzonDestinatario.agregarAEntrada(correo);
+    }
+    }   
 }
