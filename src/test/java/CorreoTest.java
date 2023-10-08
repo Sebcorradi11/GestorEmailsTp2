6609288f1a1939b37a8b437409b304dfcd9f77e9
Sebcorradi11 <sebastianocorradi57@gmail.com>
2023-10-08 18:43:42 -0300

    Test Clase Correo
    El test de la clase correo verifica que la clase correo se crea correctamente

--- src/test/java/CorreoTest.java
+++ src/test/java/CorreoTest.java
@@ -1,17 +1,33 @@
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
-
+import java.util.List;
 import org.junit.Test;
 
+import email.ucp.Contacto;
 import email.ucp.Correo;
 import email.ucp.FiltroAsuntoRemitente;
 
 
 public class CorreoTest {
     @Test
-    public void createUserTest(){// TODO
-        assertEquals(true, true);
+    public void TestCrearCorreo(){
+        Contacto remitente = new Contacto ("Sebastiano Corradi", "Sebastianocorradi57@gmail.com");
+        Contacto destinatario1 = new Contacto ("Luciano Aquino", "LucianoAquinopla@gmail.com");
+        Contacto destinatario2 = new Contacto ("Gonzalo Mata", "GonzaMataxd@gmail.com");
+
+        //Crear una lista de destinatarios
+        List<Contacto> destinatarios = new ArrayList<>();
+        destinatarios.add(destinatario1);
+        destinatarios.add(destinatario2);
+
+        Correo correo = new Correo("Asunto del Correo", "Contenido del Correo", remitente, destinatarios );
+
+        assertEquals("Asunto del Correo", correo.getAsunto());
+        assertEquals("Contenido del Correo", correo.getContenido());
+        assertEquals(remitente, correo.getRemitente());
+        assertEquals(destinatarios,correo.getDestinatarios());
     }
-}
+    }
+
     
