03accbab34559133485d6b6040a02ba91ceeb953
Sebcorradi11 <sebastianocorradi57@gmail.com>
2023-10-08 18:11:57 -0300

    CorreoClase
    Creacion de la clase correo

--- src/main/java/email/ucp/Correo.java
+++ src/main/java/email/ucp/Correo.java
@@ -1,6 +1,35 @@
 package email.ucp;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Correo {
+    private String asunto;
+    private String contenido;
+    private Contacto remitente;
+    private List<Contacto> destinatarios;
+
+
+
+    public Correo (String asunto, String contenido, Contacto remitente, List<Contacto> destinatarios){
+    this.asunto = asunto;
+    this.contenido = contenido;
+    this.remitente = remitente;
+    this.destinatarios = destinatarios;
+   } 
+   public String getAsunto() {
+    return asunto;
+   }
+    public String getContenido() {
+    return asunto;
+   }
+   
+    public String getRemitente() {
+    return asunto;
+   }
+   public List<Contacto> getDestinatarios(){
+    return destinatarios;
+   }
+   
+
 }
