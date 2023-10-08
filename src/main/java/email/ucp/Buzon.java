e79377b77369536063672eaa1b4573d6eb82f011
Sebcorradi11 <sebastianocorradi57@gmail.com>
2023-10-08 18:14:18 -0300

    Clase buzon creada
    Creacion de la clase buzon

--- src/main/java/email/ucp/Buzon.java
+++ src/main/java/email/ucp/Buzon.java
@@ -2,6 +2,26 @@ package email.ucp;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Buzon {
+    private List <Correo> bandejaEntrada;
+    private List <Correo> bandejaEnviados;
+
+    public Buzon (){
+        bandejaEntrada = new ArrayList<>();
+        bandejaEnviados = new ArrayList<>();
+    }
+    public void agregarAEntrada(Correo correo){
+        bandejaEntrada.add(correo);
+    }
+    public void agregarAEnviados(Correo correo){
+        bandejaEnviados.add(correo);
+    }
+    public List<Correo> getBandejaEntrada(){
+        return bandejaEntrada;
+    }
+    public List<Correo> getBandejaEnviados(){
+        return bandejaEnviados;
+    }
 }
