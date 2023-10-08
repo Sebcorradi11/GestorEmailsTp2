d94b2d26d03501ec71b228b9a9635e0e43445406
Sebcorradi11 <sebastianocorradi57@gmail.com>
2023-10-08 19:06:55 -0300

    ContactoTest
    Crea un contacto

--- src/test/java/ContactoTest.java
+++ src/test/java/ContactoTest.java
@@ -4,9 +4,18 @@ import java.util.ArrayList;
 
 import org.junit.Test;
 
+import email.ucp.Contacto;
 import email.ucp.Correo;
 import email.ucp.FiltroAsuntoRemitente;
 
 public class ContactoTest{
+    @Test
+    public void testCrearContacto(){
+        Contacto contacto = new Contacto ("Sebastiano Corradi", "Sebastianocorradi57@gmail.com");
 
+        assertEquals("Sebastiano Corradi", contacto.getNombre());
+        assertEquals("Sebastianocorradi57@gmail.com", contacto.getMail());
+
+    }
+    
 }
