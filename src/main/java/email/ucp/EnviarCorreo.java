a280e16a490dc58cf24aff5d2de8fde3af9df90c
Sebcorradi11 <sebastianocorradi57@gmail.com>
2023-10-09 00:57:32 -0300

    EnviarCorreo correcciones
    .

--- src/main/java/email/ucp/EnviarCorreo.java
+++ src/main/java/email/ucp/EnviarCorreo.java
@@ -1,15 +1,22 @@
 package email.ucp;
+import java.util.List;
+import java.util.ArrayList;
+
 public class EnviarCorreo {
     public static void enviar(Correo correo, Buzon buzonRemitente, Buzon buzonDestinatario) {
         if (correo == null || buzonRemitente == null || buzonDestinatario == null) {
             throw new IllegalArgumentException("Correo, buzonRemitente y buzonDestinatario no pueden ser nulos.");
         }
 
+        // Verificar que el correo esté en la bandeja de enviados del remitente
         if (!buzonRemitente.getBandejaEnviados().contains(correo)) {
             throw new IllegalArgumentException("El correo no está en la bandeja de enviados del buzonRemitente.");
         }
 
-        buzonRemitente.eliminarDeEnviados(correo);
+        // Agregar el correo a la bandeja de entrada del destinatario
         buzonDestinatario.agregarAEntrada(correo);
+
+        // Eliminar el correo de la bandeja de enviados del remitente
+        buzonRemitente.eliminarDeEnviados(correo);
     }
 }
